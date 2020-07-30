b9a9acd7826c83552d8d35002fe488060b8abfa0
Reinaldo Otalvaro <reinaldo.otalvaro@gmail.com>
2020-07-29 21:47:39 -0500

    Base project

---
+++ corrientazo-deliveries/src/main/java/co/com/s4n/deliveries/Main.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package co.com.s4n.deliveries;
+
+/**
+ * @author linux
+ *
+ */
+public class Main {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Initializing...");
+	}
+}
---
+++ corrientazo-deliveries/src/test/java/co/com/s4n/deliveries/MainTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package co.com.s4n.deliveries;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author linux
+ *
+ */
+public class MainTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
