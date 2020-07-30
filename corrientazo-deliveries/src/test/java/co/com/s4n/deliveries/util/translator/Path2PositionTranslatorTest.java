b0820bac20a6c6aa2aae24a33f43d3c1fdb785eb
Reinaldo Otalvaro <reinaldo.otalvaro@gmail.com>
2020-07-30 02:35:48 -0500

    first utest

---
+++ corrientazo-deliveries/src/test/java/co/com/s4n/deliveries/util/translator/Path2PositionTranslatorTest.java
@@ -0,0 +1,23 @@
+package co.com.s4n.deliveries.util.translator;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Path2PositionTranslatorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testTranslate() {
+		//fail("Not yet implemented");
+	}
+}
